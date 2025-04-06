57b7ba90c35d679dcd6585d2a0e32057a553c457
M <hostgeow@gmail.com>
2025-04-07 01:46:25 +0300

    Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print('hello')
+    return "<h1>Hello World!<h1>"
+
+@app.route("/sentiment", methpds=['POST'])
+def get_sentiment():
+    input_data = request.json
+    print(input_data)
+
+    return {'input_data': input_data, 'message': 'Hello!'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8080', debug=False)
