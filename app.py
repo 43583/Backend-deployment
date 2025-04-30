52c07c271bf1baff2449e456cd8b8b45fb2dc4c4
M <hostgeow@gmail.com>
2025-04-30 05:27:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-# from flask_cors import CORS
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
-# CORS(app, resources={r'*': {'origins': '*'}})
+CORS(app)
 
 @app.route("/")
 def hello_world():
